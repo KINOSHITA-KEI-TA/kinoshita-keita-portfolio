5783348846d2eb87cb3596a4fc41c20b2edfb985
KINOSHITA-KEI-TA <kei.quality3@gmail.com>
2020-09-19 02:08:58 +0900

    フォームの実装

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,26 @@
 class TopsController < ApplicationController
   def index
   end
+
+  
+  def new
+    @top = Top.new
+  end
+
+  def create
+    @top = Top.new(top_params)
+    if @top.save
+      redirect_to root_path,notice: 'メッセージが送信されました'
+    else
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :new
+    end
+  end
+
+
+  private
+  def top_params
+    params.require(:top).permit(:name,:email,:text)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'tops/index'
   root "tops#index"
+  resources :tops,only:[:new,:create]
 end
