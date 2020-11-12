cb0de850765e455fa06f5395bfe91e927cb0db15
KINOSHITA-KEI-TA <kei.quality3@gmail.com>
2020-11-12 18:37:55 +0900

    スクロールフェード機能

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,18 @@ $(function() {
       autoplaySpeed: 2000,
   });
 });
+
+$(function(){
+  $(window).scroll(function (){
+    $("#sample").each(function(){
+      var imgPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > imgPos - windowHeight + windowHeight/5){
+        $(this).addClass("fade_on");
+      } else {
+        $(this).removeClass("fade_on");
+      }
+    });
+  });
+});
